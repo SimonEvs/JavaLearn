4bfd793dee69de72d42cacd0b0dd524280bcd26c
SimonEvs <simonevseev07@mail.ru>
2022-10-25 19:06:31 +0300

    add Hanoi Towers task

--- Java_Tasks/Second/TowerOfHanoi.java
+++ Java_Tasks/Second/TowerOfHanoi.java
@@ -2,20 +2,23 @@
 
 package Java_Tasks.Second;
 
+import java.util.Scanner;
 public class TowerOfHanoi {
 
     public static void main(String[] args) {
-        int count = 3;
+        Scanner iScanner=new Scanner(System.in);
+        System.out.println("Введите число колец: ");
+        int count = iScanner.nextInt();
         moveRing('A', 'B', 'C', count);
     }
 
     public static void moveRing(char a, char b, char c, int count) {
 
         if (count == 1) {
-            System.out.println(String.format("from %s to %s | Из %s в %s", a, b, a, b));
+            System.out.println(String.format("From %s to %s | Из %s в %s", a, b, a, b));
         } else {
             moveRing(a, c, b, count - 1);
-            System.out.println(String.format("from %s to %s | Из %s в %s", a, b, a, b));
+            System.out.println(String.format("From %s to %s | Из %s в %s", a, b, a, b));
             moveRing(c, b, a, count - 1);
         }
 
